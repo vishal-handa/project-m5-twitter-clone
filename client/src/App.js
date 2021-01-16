366f746329231b3bfdabe2ce512bec7a275b5c8f
Vishal Handa <70589573+vishal-handa@users.noreply.github.com>
2021-01-16 11:59:38 -0500

    created frontend

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+      </header>
+      <p>Hello bitches</p>
+    </div>
+  );
+}
+
+export default App;
